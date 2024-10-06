02532a1f2743f2f45a01d5fea3f81a1f205d258f
Seonguke <57488386+Seonguke@users.noreply.github.com>
2024-10-06 23:40:32 +0900

    Title: 루돌프의 반란, Time: 320ms, Memory: 28MB, Status: Passed - CodeTree

--- 241006/루돌프의 반란/rudolph-rebellion.py
+++ 241006/루돌프의 반란/rudolph-rebellion.py
@@ -58,24 +58,57 @@ class CXmas():
     def dear_move(self):
 
         move_arr = []
-        for i, dear_dir in enumerate(self.dear_move_dir):
+        # for i, dear_dir in enumerate(self.dear_move_dir):
+        #
+        #     next_dear_i = self.dear[0] + dear_dir[0]
+        #     next_dear_j = self.dear[1] + dear_dir[1]
+        #     if self.array_out_check(next_dear_i,next_dear_j) == True:
+        #         for _,santa in self.santa.items():
+        #             sant_i, sant_j, prev_move_dir, santa_power, is_sleep, is_out = santa
+        #             if is_out:continue
+        #             dist = self.distance2d(self.dear[0], self.dear[1], sant_i, sant_j)
+        #             dist2 = self.distance2d(next_dear_i, next_dear_j, sant_i, sant_j)
+        #             move_arr.append([dist, sant_i, sant_j, next_dear_i, next_dear_j, i,dist2])
+        #
+        # move_arr.sort(key=lambda x: (x[0],x[-1], -x[1], -x[2]))
+        closestX, closestY, closestIdx = 10000, 10000, 0
+        rudolf=self.dear
+        pos = self.santa
+        # 살아있는 포인트 중 루돌프에 가장 가까운 산타를 찾습니다.
+        for i in range(1, self.santa_cnt + 1):
+            if self.santa[i][-1] == True:
+                continue
 
-            next_dear_i = self.dear[0] + dear_dir[0]
-            next_dear_j = self.dear[1] + dear_dir[1]
-            if self.array_out_check(next_dear_i,next_dear_j) == True:
-                for _,santa in self.santa.items():
-                    sant_i, sant_j, prev_move_dir, santa_power, is_sleep, is_out = santa
-                    if is_out:continue
-                    dist = self.distance2d(self.dear[0], self.dear[1], sant_i, sant_j)
-                    dist2 = self.distance2d(next_dear_i, next_dear_j, sant_i, sant_j)
-                    move_arr.append([dist, sant_i, sant_j, next_dear_i, next_dear_j, i,dist2])
+            currentBest = ((closestX - rudolf[0]) ** 2 + (closestY - rudolf[1]) ** 2, (-closestX, -closestY))
+            currentValue = ((pos[i][0] - rudolf[0]) ** 2 + (pos[i][1] - rudolf[1]) ** 2, (-pos[i][0], -pos[i][1]))
 
-        move_arr.sort(key=lambda x: (x[0],x[-1], -x[1], -x[2]))
+            if currentValue < currentBest:
+                closestX, closestY = pos[i][:2]
+                closestIdx = i
 
-        self.arr_2d[self.dear[0]][self.dear[1]] = None
-        _, _, _, next_dear_i, next_dear_j, move_dir,_ = move_arr[0]
-        self.dear = [next_dear_i, next_dear_j, move_dir]
-        if self.arr_2d[next_dear_i][next_dear_j] is not None and self.arr_2d[next_dear_i][next_dear_j] >0:
+        # 가장 가까운 산타의 방향으로 루돌프가 이동합니다.
+        if closestIdx:
+            move_dir=0
+            moveX = 0
+            if closestX > rudolf[0]:
+                moveX = 1
+            elif closestX < rudolf[0]:
+                moveX = -1
+
+            moveY = 0
+
+            if closestY > rudolf[1]:
+                moveY = 1
+            elif closestY < rudolf[1]:
+                moveY = -1
+            for i in range(len(self.dear_move_dir)):
+                if self.dear_move_dir[i][0] == moveX and  self.dear_move_dir[i][1] == moveY:
+                    move_dir = i
+            self.arr_2d[self.dear[0]][self.dear[1]] = None
+            self.dear = [rudolf[0] + moveX, rudolf[1] + moveY,move_dir]
+
+
+        if  self.arr_2d[self.dear[0]][self.dear[1]]is not None and  self.arr_2d[self.dear[0]][self.dear[1]] >0:
             self.dear_meet_santa()
 
         self.arr_2d[self.dear[0]][self.dear[1]] = self.dear
@@ -89,7 +122,7 @@ class CXmas():
     def dear_meet_santa(self):
         next_dear_i, next_dear_j, move_dir = self.dear
         santa_num = self.arr_2d[next_dear_i][next_dear_j]
-        #prev_santa_num = None
+
         q = deque([[None,santa_num]])
 
         while(q):
@@ -98,8 +131,8 @@ class CXmas():
             santa = self.santa[santa_num]
             sant_i, sant_j, prev_move_dir, santa_power, is_sleep, is_out = santa
 
-            if prev_santa_num == None:
-                santa_power+=self.dear_power
+            if prev_santa_num == None: #루도프랑 산타 만남
+                santa_power += self.dear_power
 
                 sant_next_i = sant_i + (self.dear_move_dir[move_dir][0]*self.dear_power)
                 sant_next_j = sant_j + (self.dear_move_dir[move_dir][1]*self.dear_power)
@@ -110,15 +143,14 @@ class CXmas():
                     is_out = True
                 else:
                     is_in = self.check_arr_2d(sant_next_i,sant_next_j)
-                    if is_in ==self.is_santa:
+                    if is_in == self.is_santa and self.arr_2d[sant_next_i][sant_next_j] != santa_num:
                         q.append([santa_num,self.arr_2d[sant_next_i][sant_next_j]])
 
                 santa = [sant_next_i,sant_next_j,prev_move_dir,santa_power,is_sleep,is_out]
                 self.santa[santa_num]=santa
-                try:
-                    self.arr_2d[sant_i][sant_j] = None
-                except:
-                    a=0
+
+                self.arr_2d[sant_i][sant_j] = None
+
                 if is_out == False:
                     self.arr_2d[sant_next_i][sant_next_j] = santa_num
             else:
@@ -136,8 +168,10 @@ class CXmas():
 
                 santa = [sant_next_i,sant_next_j,prev_move_dir,santa_power,is_sleep,is_out]
                 self.santa[santa_num]=santa
+
                 if self.arr_2d[sant_i][sant_j] == santa_num:
                     self.arr_2d[sant_i][sant_j] = None
+
                 if is_out == False:
                     self.arr_2d[sant_next_i][sant_next_j] = santa_num
 
@@ -223,7 +257,7 @@ class CXmas():
                     if is_in == self.is_santa:
                         q.append([santa_num, self.arr_2d[sant_next_i][sant_next_j]])
 
-                santa = [sant_next_i, sant_next_j, prev_move_dir, santa_power, is_sleep, is_out]
+                santa = [sant_next_i, sant_next_j, prev_dir, santa_power, is_sleep, is_out]
                 self.santa[santa_num] = santa
                 #self.arr_2d[sant_i][sant_j] = None
 
@@ -254,7 +288,7 @@ class CXmas():
 
     def game_start(self):
         for k in range(self.m):
-            if k== 6:
+            if k== 47:
                 a=0
             self.dear_move()
             self.santa_move()
@@ -268,13 +302,16 @@ class CXmas():
                 self.santa[santa_num] = [sant_i, sant_j, prev_move_dir, santa_power+1, is_sleep, is_out]
 
             # print(f"------------------{k}-----------------")
-            # for i in range(self.n):
-            #     for j in range(self.n):
-            #         print(f"{self.arr_2d[i][j]}", end=' ')
-            #     print()
+            # # for i in range(self.n):
+            # #     for j in range(self.n):
+            # #         print(f"{self.arr_2d[i][j]}", end=' ')
+            # #     print()
+            # print(f"ruldolph : {self.dear[0]+1},{self.dear[1]+1}")
             # for k,v in self.santa.items():
-            #     print(f"{k}:{v[3]}/ nuck_down {v[4]}")
-
+            #     #print(f"{k}:{v[3]}/ nuck_down {v[4]}")
+            #     print(f"({v[0]},{v[1]})",end= ' ')
+            #     print(f"{v[3]}",end=' ')
+            # print()
         for santa_num, santa in self.santa.items():
             sant_i, sant_j, prev_move_dir, santa_power, is_sleep, is_out = santa
             print(santa_power,end=' ')
